c6423e787ee0983d71d5cd7b1fcdd7b9a70950a7
pierre enoc <pierre_enoc@yahoo.fr>
2025-04-26 08:17:58 -0300

    refactor error return

--- src/main/java/com/client/api/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/api/exception/handler/ResourceHandler.java
@@ -1,6 +1,7 @@
 package com.client.api.exception.handler;
 
 
+import com.client.api.dto.error.ErrorResponseDto;
 import com.client.api.exception.BadRequestException;
 import com.client.api.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -13,14 +14,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ResourceHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException n) {
+    public ResponseEntity<ErrorResponseDto> notFoundException(NotFoundException n) {
         String errorMessage = n.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto.builder()
+                .message(n.getMessage())
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .build());
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> badRequest(BadRequestException b) {
+    public ResponseEntity<ErrorResponseDto> badRequest(BadRequestException b) {
         String errorMessage = b.getMessage();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
+                .message(b.getMessage())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
     }
 }
