c4c824f8e2e229fb519f58035feb5179c22180b6
pierre enoc <pierre_enoc@yahoo.fr>
2025-04-25 21:22:31 -0300

    add handler

--- src/main/java/com/client/api/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/api/controller/SubscriptionTypeController.java
@@ -7,10 +7,7 @@ import com.client.api.services.SubscriptionTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -29,11 +26,7 @@ public class SubscriptionTypeController {
 
     @GetMapping("/{id}")
     public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
-        try {
            return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.fidById(id));
-        } catch (NotFoundException n) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-
     }
+
 }
---
+++ src/main/java/com/client/api/exception/handler/ResourceHandler.java
@@ -0,0 +1,19 @@
+package com.client.api.exception.handler;
+
+
+import com.client.api.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ResourceHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> notFoundException(NotFoundException n) {
+        String errorMessage = n.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+}
